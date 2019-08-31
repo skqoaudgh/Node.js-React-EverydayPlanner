76b672c668bf779fac931520019ca1a4e813275e
myungho <skqoaudgh2@hanmail.net>
2019-08-31 16:27:26 +0900

    MongoDB 연결

--- server.js
+++ server.js
@@ -1,11 +1,26 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+const authRouter = require('./routers/auth');
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.use('/auth', authRouter);
 
 app.get('/', (req, res, next) => {
     res.send('Hello World!');
 });
 
-app.listen(5000, function() {
-    console.log('express server is opened on port 5000');
+mongoose.connect('mongodb+srv://' + process.env.MONGO_USER + ':' + process.env.MONGO_PASSWORD + '@node-rest-shop-zqnku.mongodb.net/' + process.env.MONGO_DB + '?retryWrites=true', 
+{ useNewUrlParser: true})
+.then( () =>  {
+    app.listen(8000, () => {
+        console.log('server is opened on port 8000');
+    });
+})
+.catch(err=> {
+    console.log(err);
 });
