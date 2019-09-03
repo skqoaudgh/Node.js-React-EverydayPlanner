632c30b5e754d80b24915787084653229ec7aac5
myungho <skqoaudgh2@hanmail.net>
2019-09-03 13:47:52 +0900

    Auth 컨텍스트, 네비게이션 컴포넌트 추가

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
+
+import MainNavigation from './components/Navigation/MainNavigation';
+import AuthContext from './context/authContext';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      Hello World
-    </div>
-  );
+class App extends Component {
+  state = {
+    token: null,
+    userId: null,
+    userEmail: null
+  }
+
+  login = (token, userId, email) => {
+    this.setState({token: token, userId: userId, userEmail: email});
+  };
+  
+  logout = () => {
+    this.setState({token: null, userId: null, userEmail: null});
+  };
+
+  render() {
+    return (
+      <BrowserRouter>
+      <AuthContext.Provider
+          value={{
+            token: this.state.token, 
+            userId: this.state.userId, 
+            userEmail: this.state.userEmail,
+            login: this.login,
+            logout: this.logout
+          }}>
+          <MainNavigation />
+          <main class="main-content">
+            <Switch>
+
+            </Switch>
+          </main>
+        </AuthContext.Provider>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
---
+++ frontend/src/components/Navigation/MainNavigation.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+import AuthContext from '../../context/authContext';
+
+import './MainNavigation.css';
+
+const mainNavigation = props => (
+    <AuthContext.Consumer>
+        {(context) => {
+            return (
+                <header className="main-navigation">
+                    <div className="main-navigation__logo">
+                        <h1>EVERYDAY</h1>
+                    </div>
+                    <div className="main-navigation__item">
+                        <ul>
+                        </ul>
+                    </div>
+                </header>  
+            );
+        }}
+    </AuthContext.Consumer>
+);
+
+export default mainNavigation;
