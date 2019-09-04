75c964e92061bdf811c856abc3082fcae9dc8c84
myungho <skqoaudgh2@hanmail.net>
2019-09-04 14:07:48 +0900

    계정 로그인 정보가 계속 유지되게 추가

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import MainNavigation from './components/Navigation/MainNavigation';
 import AuthContext from './context/authContext';
 
 import AuthPage from './pages/Auth';
+import PlannerPage from './pages/Planner';
 
 import './App.css';
 
@@ -23,7 +24,25 @@ class App extends Component {
     this.setState({token: null, userId: null, userEmail: null});
   };
 
+  UNSAFE_componentWillMount() {
+    const savedToken = localStorage.savedToken;
+    const savedUserId = localStorage.savedUserId;
+    const savedUserEmail = localStorage.saveduserEmail;
+    if(savedToken !== 'null' && savedUserId !== 'null' && savedUserEmail !== 'null') {
+      this.setState({token: savedToken, userId: savedUserId, userEmail: savedUserEmail});
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if(prevState.token !== this.state.token && prevState.userId !== this.state.userId && prevState.userEmail !== this.state.userEmail) {
+        localStorage.savedToken = this.state.token;
+        localStorage.savedUserId = this.state.userId;
+        localStorage.savedUserEmail = this.state.userEmail;
+      }
+  }
+
   render() {
+    console.log(this.state.token);
     return (
       <BrowserRouter>
       <AuthContext.Provider
@@ -37,7 +56,10 @@ class App extends Component {
           <MainNavigation />
           <main className="main-content">
             <Switch>
+              {this.state.token && <Redirect path="/" to="/my" exact/>}
+              {this.state.token && <Redirect path="/auth" to="/my" exact/>}
               {!this.state.token && <Route path="/auth" component={AuthPage}/>}
+              {this.state.token && <Route path="/my" component={PlannerPage}/>}
               {!this.state.token && <Redirect to="/auth" exact/>}
             </Switch>
           </main>
