77cb28dbb8fe5e89f538bba942e559d83e58b42a
myungho <skqoaudgh2@hanmail.net>
2019-09-05 14:29:47 +0900

    반복 옵션에 '매일' 추가

--- frontend/src/components/RepeatOption/RepeatOption.js
+++ frontend/src/components/RepeatOption/RepeatOption.js
@@ -6,10 +6,12 @@ const repeatoption = props => (
     <div className="repeatOption-Container">
         <input className="magic-radio" type="radio" id="a" name="repeatOption" value="NoRepeat" onChange={props.onChange} />
         <label htmlFor="a">No Repeat</label>
-        <input className="magic-radio" type="radio" id="b" name="repeatOption" value="Weekly" onChange={props.onChange} />
-        <label htmlFor="b">Weekly</label>
-        <input className="magic-radio" type="radio" id="c" name="repeatOption" value="Monthly" onChange={props.onChange} />
-        <label htmlFor="c">Monthly</label>
+        <input className="magic-radio" type="radio" id="b" name="repeatOption" value="Daily" onChange={props.onChange} />
+        <label htmlFor="b">Daily</label>  
+        <input className="magic-radio" type="radio" id="c" name="repeatOption" value="Weekly" onChange={props.onChange} />
+        <label htmlFor="c">Weekly</label>
+        <input className="magic-radio" type="radio" id="d" name="repeatOption" value="Monthly" onChange={props.onChange} />
+        <label htmlFor="d">Monthly</label>
     </div>
 )
 
