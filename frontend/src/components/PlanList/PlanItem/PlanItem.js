afb32aeecb9b5be33449b5db6c2f890df85f3b72
myungho <skqoaudgh2@hanmail.net>
2019-09-05 17:10:33 +0900

    계획의 상세보기 드롭다운 추가

--- frontend/src/components/PlanList/PlanItem/PlanItem.js
+++ frontend/src/components/PlanList/PlanItem/PlanItem.js
@@ -4,6 +4,27 @@ import './PlanItem.css'
 
 import alt from '../../../materials/icons/alt.png';
 
+const checkOneSelection = event => {
+    const allList = document.getElementsByClassName('toggle');
+    if(event.target.checked) {
+        for(let i=0; i<allList.length; i++) {
+            if(allList[i] === event.target) continue;
+            if(allList[i].checked) {
+                allList[i].checked = false;
+            }
+        }
+    }
+}
+
+function formatDate(date) { 
+    var d = new Date(date), month = '' + (d.getMonth() + 1), day = '' + d.getDate(), year = d.getFullYear(); 
+    if (month.length < 2) 
+        month = '0' + month; 
+    if (day.length < 2) 
+        day = '0' + day; 
+    return [year, month, day].join('-'); 
+}
+
 const planItem = props => (
     <li key={props.id} className="plans__list-item">
         <div className="PlanItem-Container">
@@ -12,6 +33,19 @@ const planItem = props => (
             </div>
             <div className="PlanBody-Container">
                 <h2>{props.title}</h2>
+            </div>
+            <div class="dropdown">
+                <input type="checkbox" id={props.id} name="toggle" className="toggle" onChange={checkOneSelection}/>
+                <label htmlFor={props.id} className="toggleLabel"></label>
+                <div className="message">
+                    <div className="detail-container">
+                        <div className="plan-title">{props.title}</div>
+                        -
+                        <div className="plan-date">{formatDate(props.date)}~{formatDate(props.deadline)}</div>
+                    </div>
+                    <p className="plan-detail">{props.detail}</p>
+                    <div class="delButton-container"><button>Delete</button></div>
+                </div>
             </div>
         </div>
         <hr />
