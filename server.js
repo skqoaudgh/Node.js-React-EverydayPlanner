828aa79c79b2735e53e078b167ff5f4dc6cad027
myungho <skqoaudgh2@hanmail.net>
2019-09-11 12:35:52 +0900

    CORS 가능한 헤더 옵션 추가 (DELETE ,PUT)

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.urlencoded({extended: true}));
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
+    res.setHeader('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, DELETE, PUT');
     res.setHeader('Access-Control-Allow-Headers', 'Content-type, Authorization');
     if(req.method === 'OPTIONS') {
         return res.sendStatus(200);
