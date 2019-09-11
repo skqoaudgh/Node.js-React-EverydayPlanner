0546547c5c8702621600e0fbe4e6c66df9d39b24
myungho <skqoaudgh2@hanmail.net>
2019-09-11 12:18:45 +0900

    계획 수정 및 삭제 API 추가

--- controllers/myPlan.js
+++ controllers/myPlan.js
@@ -44,5 +44,50 @@ module.exports = {
         catch(err) {
             throw err;
         }
+    },
+    deletePlan: async (req, res, next) => {
+        if(!req.isAuth) {
+            return res.status(200).json({result: 'authError'});
+        }
+
+        const id = req.params.id;
+
+        const deletedPlan = await Plan.deleteOne({_id: id});
+        if(!deletedPlan) {
+            return res.status(200).json({result: 'error'});
+        }
+
+        return res.status(200).json({result: 'done'});
+    },
+    updatePlan: async (req, res, next) => {
+        if(!req.isAuth) {
+            return res.status(200).json({result: 'authError'});
+        }
+
+        const id = req.params.id;
+
+        const repeatOption = req.body.repeatOption;
+        const title = req.body.title;
+        const detail = req.body.detail;
+        const marker = req.body.marker;
+        const deadline = req.body.deadline;
+
+        const plan = await Plan.findById(id);
+        if(!plan) {
+            return res.status(200).json({result: 'error'});
+        }
+
+        plan.RepeatOption = repeatOption;
+        plan.Title = title;
+        plan.Detail = detail;
+        plan.Marker = marker;
+        plan.Deadline = deadline;
+
+        const updatedPlan = await plan.save();
+        if(!updatedPlan) {
+            return res.status(200).json({result: 'error'});
+        }
+
+        return res.status(200).json({result: 'done'});
     }
 }
--- routers/myPlan.js
+++ routers/myPlan.js
@@ -4,5 +4,7 @@ const router = express.Router();
 
 router.get('/', planController.getMyPlans);
 router.post('/', planController.addMyPlan);
+router.delete('/:id', planController.deletePlan);
+router.put('/:id', planController.updatePlan);
 
 module.exports = router;
