f7513c17f068a957472de9b37bacfd3568af9930
myungho <skqoaudgh2@hanmail.net>
2019-09-15 13:14:29 +0900

    모든 컨트롤러에 try-catch문 추가

--- controllers/myPlan.js
+++ controllers/myPlan.js
@@ -47,74 +47,89 @@ module.exports = {
         }
     },
     deletePlan: async (req, res, next) => {
-        if(!req.isAuth) {
-            return res.status(200).json({result: 'authError'});
+        try {
+            if(!req.isAuth) {
+                return res.status(200).json({result: 'authError'});
+            }
+
+            const id = req.params.id;
+
+            const deletedPlan = await Plan.deleteOne({_id: id});
+            if(!deletedPlan) {
+                return res.status(200).json({result: 'error'});
+            }
+
+            return res.status(200).json({result: 'done'});
         }
-
-        const id = req.params.id;
-
-        const deletedPlan = await Plan.deleteOne({_id: id});
-        if(!deletedPlan) {
-            return res.status(200).json({result: 'error'});
+        catch(err) {
+            throw err;
         }
-
-        return res.status(200).json({result: 'done'});
     },
     updatePlan: async (req, res, next) => {
-        if(!req.isAuth) {
-            return res.status(200).json({result: 'authError'});
+        try {
+            if(!req.isAuth) {
+                return res.status(200).json({result: 'authError'});
+            }
+
+            const id = req.params.id;
+
+            const repeatOption = req.body.repeatOption;
+            const title = req.body.title;
+            const detail = req.body.detail;
+            const marker = req.body.marker;
+            const deadline = req.body.deadline;
+
+            const plan = await Plan.findById(id);
+            if(!plan) {
+                return res.status(200).json({result: 'error'});
+            }
+
+            plan.RepeatOption = repeatOption;
+            plan.Title = title;
+            plan.Detail = detail;
+            plan.Marker = marker;
+            plan.Deadline = deadline;
+
+            const updatedPlan = await plan.save();
+            if(!updatedPlan) {
+                return res.status(200).json({result: 'error'});
+            }
+
+            return res.status(200).json({result: 'done'});
         }
-
-        const id = req.params.id;
-
-        const repeatOption = req.body.repeatOption;
-        const title = req.body.title;
-        const detail = req.body.detail;
-        const marker = req.body.marker;
-        const deadline = req.body.deadline;
-
-        const plan = await Plan.findById(id);
-        if(!plan) {
-            return res.status(200).json({result: 'error'});
+        catch(err) {
+            throw err;
         }
-
-        plan.RepeatOption = repeatOption;
-        plan.Title = title;
-        plan.Detail = detail;
-        plan.Marker = marker;
-        plan.Deadline = deadline;
-
-        const updatedPlan = await plan.save();
-        if(!updatedPlan) {
-            return res.status(200).json({result: 'error'});
-        }
-
-        return res.status(200).json({result: 'done'});
     }, 
     checkPlan: async (req, res ,next) => {
-        if(!req.isAuth) {
-            return res.status(200).json({result: 'authError'});
-        }
-        
-        const id = req.params.id;
-        const plan = req.body.plan;
-        const date = req.body.date;
+        try {
+            if(!req.isAuth) {
+                return res.status(200).json({result: 'authError'});
+            }
+            
+            const id = req.params.id;
+            const plan = req.body.plan;
+            const date = req.body.date;
 
-        const isExist = Check.findOne({Creator: id, Plan: plan, Date: date});
-        if(!isExist) {
-            const newCheck = new Check({
-                Creator: id,
-                Plan: plan,
-                isChecked: true
-            });
-    
-            await newCheck.save();
-            return res.status(201).json({result: 'done'});
+            const isExist = Check.findOne({Creator: id, Plan: plan, Date: date});
+            if(!isExist) {
+                const newCheck = new Check({
+                    Creator: id,
+                    Plan: plan,
+                    isChecked: true
+                });
+        
+                await newCheck.save();
+                return res.status(201).json({result: 'done'});
+            }
+            else {
+                isExist.isChecked = false;
+                await newCheck.save();
+                return res.status(201).json({result: 'done'});
+            }
         }
-        else {
-            isExist.isChecked = false;
-            await newCheck.save();
-            return res.status(201).json({result: 'done'});
+        catch(err) {
+            throw err;
         }
     }
 }
